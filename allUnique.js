60d6d0745739ac2e55cc7fd4537559672ffff923
cli333 <cml8198@protonmail.com>
2020-03-02 11:39:41 -0800

    a

---
+++ allUnique.js
@@ -0,0 +1,14 @@
+// determine if a string has all unique chars
+// cannot use extra space/data structures
+
+const unique = string => {
+  // brute force
+  // for (let i = 0; i < string.length; i++) {
+  //   for (let j = 1; j < string.length; j++) {
+  //     if (string[i] === string[j]) return false;
+  //   }
+  // }
+  // return true;
+};
+
+console.log(unique("abacd"));
