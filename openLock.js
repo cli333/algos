7aac318ede395ffb96d84230d94369bed23fa654
cli333 <cml8198@protonmail.com>
2020-03-30 13:27:50 -0700

    a

--- openLock.js
+++ openLock.js
@@ -3,9 +3,7 @@ You have a lock in front of you with 4 circular wheels.Each wheel has 10 slots: 
 rotate freely and wrap around: for example we can turn '9' to be '0', or '0' to be '9'.Each move consists of turning one wheel one slot.
 
 The lock initially starts at '0000', a string representing the state of the 4 wheels.
-
 You are given a list of deadends dead ends, meaning if the lock displays any of these codes, the wheels of the lock will stop turning and you will be unable to open it.
-
 Given a target representing the value of the wheels that will unlock the lock, return the minimum total number of turns required to open the lock, or - 1 if it is impossible.
 
 Example 1:
@@ -50,6 +48,7 @@ const openLock = (deadends, target) => {
 
   while (queue.length) {
     let size = queue.length;
+
     while (size > 0) {
       // 'pop' off
       let lock_position = queue.shift();
@@ -67,6 +66,7 @@ const openLock = (deadends, target) => {
         const current_position = combo[i];
         // for each position increment and decrement
         // push the 'combo' to the queue
+        //
         // increment the lock
         let s1 =
           combo.substring(0, i) +
@@ -94,6 +94,7 @@ const openLock = (deadends, target) => {
       }
       size--;
     }
+    // whenever inner loop is run, increment the moves
     moves++;
   }
   return -1;
@@ -106,4 +107,56 @@ console.log(
     ["8887", "8889", "8878", "8898", "8788", "8988", "7888", "9888"],
     "8888"
   )
-);
+); // -1
+
+const open2 = (deadends, target) => {
+  let dead = deadends.reduce((a, b) => {
+    a[b] = true;
+    return a;
+  }, {});
+  const q = ["0000"];
+  const visited = { "0000": true };
+  let moves = 0;
+
+  while (q.length) {
+    let size = q.length;
+    while (size > 0) {
+      let currentCombo = q.shift();
+      if (dead[currentCombo]) {
+        size--;
+        continue;
+      }
+      if (currentCombo === target) {
+        return moves;
+      }
+      for (let i = 0; i < 4; i++) {
+        let s1 =
+          currentCombo.substring(0, i) +
+          (currentCombo[i] === "9"
+            ? "0"
+            : String(Number(currentCombo[i]) + 1)) +
+          currentCombo.substring(i + 1);
+        let s2 =
+          currentCombo.substring(0, i) +
+          (currentCombo[i] === "0"
+            ? "9"
+            : String(Number(currentCombo[i]) - 1)) +
+          currentCombo.substring(i + 1);
+        if (!visited[s1] && !dead[s1]) {
+          q.push(s1);
+          visited[s1] = true;
+        }
+        if (!visited[s2] && !dead[s2]) {
+          q.push(s2);
+          visited[s2] = true;
+        }
+      }
+      size--;
+    }
+    moves++;
+  }
+
+  return -1;
+};
+
+console.log(open2(["0201", "0101", "0102", "1212", "2002"], "0202")); // 6
