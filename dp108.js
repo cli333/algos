834c07a5a88c73f629ebc073e624ea768e3478c4
cli333 <cml8198@protonmail.com>
2020-07-18 10:06:01 -0700

    a

---
+++ dp108.js
@@ -0,0 +1,68 @@
+// Given a list accounts, each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+
+// Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some email that is common to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+
+// After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+
+// Example 1:
+// Input:
+// accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
+// Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],  ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
+// Explanation:
+// The first and third John's are the same person as they have the common email "johnsmith@mail.com".
+// The second John and Mary are different people as none of their email addresses are used by other accounts.
+// We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'],
+// ['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+
+function sol(accounts) {
+  const graph = {};
+  const emailToName = {};
+
+  for (let account of accounts) {
+    const [name, ...emails] = account;
+    for (let i = 0; i < emails.length; i++) {
+      emailToName[emails[i]] = name;
+      if (i > 0) {
+        graph[emails[i - 1]]
+          ? graph[emails[i - 1]].push(emails[i])
+          : (graph[emails[i - 1]] = [emails[i]]);
+        graph[emails[i]]
+          ? graph[emails[i]].push(emails[i - 1])
+          : (graph[emails[i]] = [emails[i - 1]]);
+      }
+    }
+  }
+
+  const visited = new Set();
+  const res = [];
+  for (let mail in emailToName) {
+    if (!visited.has(mail)) {
+      visited.add(mail);
+      const list = [mail];
+      helper(mail, list);
+      list.unshift(emailToName[mail]);
+      res.push(list);
+    }
+  }
+  return res;
+
+  function helper(mail, list) {
+    if (!graph[mail] || !graph[mail].length) return;
+    for (let next of graph[mail]) {
+      if (!visited.has(next)) {
+        visited.add(next);
+        list.push(next);
+        helper(next, list);
+      }
+    }
+  }
+}
+
+console.log(
+  sol([
+    ["John", "johnsmith@mail.com", "john00@mail.com"],
+    ["John", "johnnybravo@mail.com"],
+    ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+    ["Mary", "mary@mail.com"],
+  ])
+);
---
+++ dp109.js
@@ -0,0 +1,115 @@
+// You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer, which may or may not point to a separate doubly linked list.These child lists may have one or more children of their own, and so on, to produce a multilevel data structure, as shown in the example below.
+
+// Flatten the list so that all the nodes appear in a single - level, doubly linked list.You are given the head of the first level of the list.
+
+//   Example 1:
+
+// Input: head = [1, 2, 3, 4, 5, 6, null, null, null, 7, 8, 9, 10, null, null, 11, 12]
+// Output: [1, 2, 3, 7, 8, 11, 12, 9, 10, 4, 5, 6]
+// Explanation:
+
+// The multilevel linked list in the input is as follows:
+
+// After flattening the multilevel linked list it becomes:
+
+// Example 2:
+
+// Input: head = [1, 2, null, 3]
+// Output: [1, 3, 2]
+// Explanation:
+
+// The input multilevel linked list is as follows:
+
+// 1-- - 2-- - NULL
+//   |
+//   3-- - NULL
+
+// How multilevel linked list is represented in test case:
+
+// We use the multilevel linked list from Example 1 above:
+
+// 1-- - 2-- - 3-- - 4-- - 5-- - 6--NULL
+//      |
+//      7-- - 8-- - 9-- - 10--NULL
+//            |
+//            11--12--NULL
+// The serialization of each level is as follows:
+
+// [1, 2, 3, 4, 5, 6, null]
+// [7, 8, 9, 10, null]
+// [11, 12, null]
+// To serialize all levels together we will add nulls in each level to signify no node connects to the upper node of the previous level.The serialization becomes:
+
+// [1, 2, 3, 4, 5, 6, null]
+// [null, null, 7, 8, 9, 10, null]
+// [null, 11, 12, null]
+// Merging the serialization of each level and removing trailing nulls we obtain:
+
+// [1, 2, 3, 4, 5, 6, null, null, null, 7, 8, 9, 10, null, null, 11, 12]
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+    this.child = null;
+  }
+}
+
+const a = new Node(1);
+const b = new Node(2);
+const c = new Node(3);
+const d = new Node(7);
+const e = new Node(8);
+const f = new Node(11);
+
+a.next = b;
+b.prev = a;
+b.next = c;
+c.prev = b;
+b.child = d;
+d.next = e;
+e.prev = d;
+e.child = f;
+
+function sol(head) {
+  /**
+   * // Definition for a Node.
+   * function Node(val,prev,next,child) {
+   *    this.val = val;
+   *    this.prev = prev;
+   *    this.next = next;
+   *    this.child = child;
+   * };
+   */
+  if (!head) return head;
+  const stack = [];
+  let cur = head;
+  while (cur) {
+    if (cur.child) {
+      if (cur.next) {
+        stack.push(cur.next);
+      }
+      cur.next = cur.child;
+      if (cur.next) {
+        cur.next.prev = cur;
+      }
+      cur.child = null;
+    } else if (!cur.next && !stack.length) {
+      cur.next = stack.pop();
+      if (cur.next) {
+        cur.next.prev = cur;
+      }
+    }
+    cur = cur.next;
+  }
+  return head;
+}
+
+function print(head, list = []) {
+  if (!head) return list;
+  list.push(head.val);
+  return print(head.next, list);
+}
+
+console.log(print(sol(a)), [1, 2, 7, 8, 11, 3]);
